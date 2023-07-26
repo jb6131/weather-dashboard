a52f08f5bfdf83eead54b0d861ee2e3606e7663e
jb6131 <44068903+jb6131@users.noreply.github.com>
2023-07-25 21:15:52 -0500

    added five day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,32 @@
-function getWeatherData(city) {
+function getCoordinates(city, weatherDataCallback) {
   var apiKey = '761c649664695b812876f6334b403542';
-
   var geocodingApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 
-  return fetch(geocodingApiUrl)
+  fetch(geocodingApiUrl)
     .then(function (response) {
-      console.log('Geocoding API Response:', response);
       return response.json();
     })
     .then(function (geocodingData) {
-      console.log('Geocoding Data:', geocodingData);
       if (geocodingData.length === 0) {
         showError("City not found");
-        return;
       }
 
       var lat = geocodingData[0].lat;
       var lon = geocodingData[0].lon;
-    
-      var weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-      
-      return fetch(weatherApiUrl)
-        .then(function (response) {
-          console.log('Weather API Response:', response);
-          return response.json();
-        })
-        .catch(function (error) {
-          console.error(error);
-          throw error;
-        });
+      weatherDataCallback({ lat, lon });
+    });
+}
+
+function getWeatherByCoordinates(lat, lon, weatherDataCallback) {
+  var apiKey = '761c649664695b812876f6334b403542';
+  var weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+  fetch(weatherApiUrl)
+    .then(function (response) {
+      return response.json();
     })
-    .catch(function(error) {
-      console.error(error);
-      throw error;
+    .then(function (weatherData) {
+      weatherDataCallback(weatherData);
     });
 }
 
@@ -41,8 +35,8 @@ function updateCurrentWeather(weatherData) {
   var date = '7/23/2023'; // Placeholder
   var icon = weatherData.list[0].weather[0].icon;
   var temperature = weatherData.list[0].main.temp;
-  var humidity = weatherData.list[0].main.humidity;
   var windSpeed = weatherData.list[0].wind.speed;
+  var humidity = weatherData.list[0].main.humidity;
 
   var currentWeatherDiv = document.getElementById('current-weather');
 
@@ -52,6 +46,26 @@ function updateCurrentWeather(weatherData) {
   <p>Wind: ${windSpeed} m/s</p>
   <p>Humidity: ${humidity}%</p>
   `;
+}
+
+function updateFiveDayForecast(weatherData) {
+  var forecastDivs = document.querySelectorAll('.forecast-box');
+
+  for (var i = 0; i < 5; i++) {
+    var date = '7/23/2023';
+    var icon = weatherData.list[i].weather[0].icon;
+    var temperature = weatherData.list[i].main.temp;
+    var windSpeed = weatherData.list[i].wind.speed;
+    var humidity = weatherData.list[i].main.humidity;
+
+    forecastDivs[i].innerHTML = `
+      <p>Date: ${date}</p>
+      <p><img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon"></p>
+      <p>Temp: ${temperature} K</p>
+      <p>Wind: ${windSpeed} m/s</p>
+      <p>Humidity: ${humidity}%</p>
+    `;
+  }
 }
 
 function displaySearchHistory(searchHistory) {
@@ -66,38 +80,46 @@ function displaySearchHistory(searchHistory) {
   searchHistoryDiv.innerHTML = historyButton;
 }
 
+function handleCoordinates(coordinates) {
+  getWeatherByCoordinates(coordinates.lat, coordinates.lon, 
+    function (weatherData) {
+      handleWeatherData(weatherData, coordinates.city);
+    });
+}
+
+function handleWeatherData(weatherData, city) {
+  updateCurrentWeather(weatherData);
+  updateFiveDayForecast(weatherData);
+
+  var searchHistory = localStorage.getItem('searchHistory');
+
+  if (!searchHistory) {
+    searchHistory = [];
+  } else {
+    searchHistory = JSON.parse(searchHistory);
+  }
+
+  if (!searchHistory.includes(city)) {
+    searchHistory.push(city);
+  }
+
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+
+  displaySearchHistory(searchHistory);
+}
+
 function handleSearch() {
   var searchInput = document.getElementById('city-search');
   var city = searchInput.value;
 
-  var weatherDataPromise = getWeatherData(city);
-
-  weatherDataPromise
-    .then(function(weatherData) {
-      console.log('Weather Data:', weatherData);
-      
-      updateCurrentWeather(weatherData);
-      // updateFiveDayForecast(weatherData);
-
-      var searchHistory = localStorage.getItem('searchHistory');
-
-      if (!searchHistory) {
-        searchHistory = [];
-      } else {
-        searchHistory = JSON.parse(searchHistory);
-      }
-
-      if (!searchHistory.includes(city)) {
-        searchHistory.push(city);
-      }
-
-      localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-
-      displaySearchHistory(searchHistory);
-    })
-    .catch(function(error) {
-      console.error(error);
-    });
+  getCoordinates(city, function (coordinates) {
+    var data = {
+      lat: coordinates.lat,
+      lon: coordinates.lon,
+      city: city
+    };
+    handleCoordinates(data);
+  })
 }
 
 function showError(message) {
